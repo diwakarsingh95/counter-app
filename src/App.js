e368fa94651d34a2bb3420879bd4e5e2ae4d4e6f
Diwakar <diwakar.singh313@gmail.com>
2022-09-21 14:21:39 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import styles from './App.module.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const handleIncrement = () => {
+    setCount((state) => state + 1)
+  }
+
+  const handleDecrement = () => {
+    if (count) setCount((state) => state - 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className={styles.container}>
+      <section className={styles.card}>
+        <h1 className={styles.cardTitle}>Counter</h1>
+        <div className={styles.cardBody}>
+          <button
+            onPointerDown={handleDecrement}
+            className={`${styles.btn} ${styles.red}`}
+            disabled={!count}
+          >
+            -
+          </button>
+          <span className={styles.count}>{count}</span>
+          <button
+            onClick={handleIncrement}
+            className={`${styles.btn} ${styles.blue}`}
+          >
+            +
+          </button>
+        </div>
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
